80d141b5834ac92601e693111cb6838003969ba7
Suraj Atkari <surajatkari@yahoo.com>
2022-11-14 00:59:20 +0530

    in views.py we use django shortcut import get_object_or_404 hence slug=slug

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -17,7 +17,7 @@ def book_details(request, slug):
     #     book = Book.objects.get(pk=slug)
     # except:
     #     raise Http404()
-    book = get_object_or_404(Book, slug)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_details.html", {
         "title": book.title,
         "author": book.author,
