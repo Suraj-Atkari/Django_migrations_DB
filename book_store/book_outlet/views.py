13cf289b31399464ba95131a917bd3c5c78780e8
Suraj Atkari <surajatkari@yahoo.com>
2022-11-14 01:09:23 +0530

    views.py is interated to show number of boks and average rating of the books

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
 from django.http import Http404
+from django.db.models import Avg
 
 # Create your views here.
 
 
 def index(request):
     books = Book.objects.all()
+    num_book = books.count()
+    avg_rating = books.aaggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books": num_book,
+        "average_rating": avg_rating
     })
 
 
