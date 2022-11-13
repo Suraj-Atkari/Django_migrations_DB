7b15ed9cd7f0c5dd600e1f69dd047a69638cb3d5
Suraj Atkari <surajatkari@yahoo.com>
2022-11-14 01:38:42 +0530

    add order_by to sort the index list of books

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -7,7 +7,7 @@ from django.db.models import Avg
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-title")
     num_book = books.count()
     avg_rating = books.aggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
