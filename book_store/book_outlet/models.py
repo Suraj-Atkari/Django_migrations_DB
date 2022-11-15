6e0e7159c3b6678081dc1bff4ba4719bcdbb7a9e
Suraj Atkari <surajatkari@yahoo.com>
2022-11-15 23:06:34 +0530

    done setting on Admin panel view and remane

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -10,6 +10,12 @@ class Country(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=2)
 
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
 
 class Address(models.Model):
     street = models.CharField(max_length=100)
@@ -47,7 +53,7 @@ class Book(models.Model):
         Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
-    published_country = models.ManyToManyField(Country)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-details", args=[self.slug])
